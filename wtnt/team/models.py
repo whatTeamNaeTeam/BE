3ba22720b1e18df3639bf4e6fc9797ed6c046413
fnzksxl <fnzksxl@gmail.com>
2024-03-25 18:51:08 +0900

    #4 Feat: Team 모델 생성

--- wtnt/team/models.py
+++ wtnt/team/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
+from django.db import models
+
+from core.models import TimestampedModel
+from user.models import CustomUser
 
 # Create your models here.
+
+
+class Team(TimestampedModel):
+    leader = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+    explain = models.CharField(max_length=100)
+    genre = models.CharField(max_length=5)
+    like = models.IntegerField()
+    version = models.IntegerField()
+    view = models.IntegerField()
+    is_approved = models.BooleanField(default=False)
