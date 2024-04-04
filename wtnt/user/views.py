ca958e8ec671c2083664a81133df7b4a362d7e57
fnzksxl <fnzksxl@gmail.com>
2024-04-04 17:22:35 +0900

    #7 Feat: Github 소셜 로그인 콜백, 로그인 뷰 작성
    콜백 뷰는 추후에 프론트 쪽에서 처리해주겠으나 테스트를 위해 작성

--- wtnt/user/views.py
+++ wtnt/user/views.py
@@ -1,3 +1,63 @@
-# from django.shortcuts import render
+from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+from dj_rest_auth.registration.views import SocialLoginView
+from django.contrib.auth import get_user_model
+
+import requests
+import json
 
 # Create your views here.
+
+User = get_user_model()
+
+class GithubLoginView(SocialLoginView):
+    adapter_class = GitHubOAuth2Adapter
+    callback_url = "http://localhost:8000/api/user/github/callback"
+    client_class = OAuth2Client
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        response_data = response.data
+        
+        if not response_data["user"]["email"]:
+            request_data = json.loads(request.body)
+
+            user_id = response_data["user"]["pk"]
+            extra_data = SocialAccount.objects.get(user_id=user_id).extra_data
+        
+            user = User.objects.get(id=user_id)
+            user.student_num = str(request_data.get("student_num"))
+            user.name = request_data.get("name")
+            user.email = extra_data.get("login") + "@github.com"
+            user.image = extra_data.get("avatar_url")
+            user.save()
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class GithubOAuthCallBackView(APIView):
+    def get(self, request: Request):
+        if code := request.GET.get("code"):
+            response = self.send_code_to_github_login_view(code)
+            if response.status_code == 200:
+                return Response(response.json(), status=status.HTTP_200_OK)
+            return Response(
+                {"error": "Failed to process with GithubLoginView"},
+                status=response.status_code,
+            )
+        
+    def send_code_to_github_login_view(self, code: str):
+        url = "http://localhost:8000/api/user/github/login"
+        payload = {"code": code}
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, json=payload, headers=headers)
+
+        return response
