8c7dfa0258c3157ea37524bf9fe268c6f2681211
fnzksxl <fnzksxl@gmail.com>
2024-04-08 02:25:13 +0900

    #7 Fix: 미들웨어 수정

--- wtnt/core/middleware.py
+++ wtnt/core/middleware.py
@@ -4,35 +4,45 @@ from django.utils.deprecation import MiddlewareMixin
 class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.APIS = ['github/login', 'github/finish', 'token/refresh']
-    
+        self.APIS = ["github/login", "github/finish"]
+        self.REFRESH = "token/refresh"
+
     def process_response(self, request, response):
         path = request.path_info
         is_valid = any(api in path for api in self.APIS)
+        is_refresh = True if self.REFRESH in path else False
 
-        if is_valid:
-            if request.META.get('HTTP_FROM', None) == 'web':
-                response.set_cookie('refresh', response.data['refresh'])
-                response.set_cookie('access', response.data['access'])
-                
-                response.data.pop('access')
-                response.data.pop('refresh')
+        if is_valid or is_refresh:
+            if request.META.get("HTTP_FROM", None) == "web":
+                print(response.data)
+                response.set_cookie("refresh", response["refresh"])
+                response.set_cookie("access", response["access"])
+
+                if is_valid:
+                    response.data.pop("access")
+                    response.data.pop("refresh")
 
                 response.content = response.render().rendered_content
-        
+
         return response
 
 
 class AttachJWTFromCookieToHeaderMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.NOT_APIS = ['github/login', 'github/finish', 'token/refresh']
+        self.NOT_APIS = ["github/login", "github/finish"]
+        self.REFRESH = "token/refresh"
 
     def process_request(self, request):
         path = request.path_info
         is_valid = any(api in path for api in self.NOT_APIS)
+        is_refresh = True if self.REFRESH in path else False
 
-        if is_valid:
-            if request.META.get('HTTP_FROM', None) == 'web':
-                request.META['access'] = request.COOKIES.get('access', None)
-                request.META['refresh'] = request.COOKIES.get('refresh', None)
+        if not is_valid:
+            if request.META.get("HTTP_FROM", None) == "web":
+                request.META["access"] = request.COOKIES.get("access", None)
+                request.META["refresh"] = request.COOKIES.get("refresh", None)
+
+        if is_refresh:
+            if request.META.get("HTTP_FROM", None) == "web":
+                request.META["refresh"] = request.COOKIES.get("refresh", None)
