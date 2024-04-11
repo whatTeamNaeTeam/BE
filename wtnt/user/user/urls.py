5ee6b5f87b047d84e02715ea89988c6b16140933
fnzksxl <fnzksxl@gmail.com>
2024-04-11 17:37:50 +0900

    #11 Feat: Path Query 사용할 수 있게 urls 수정

--- wtnt/user/user/urls.py
+++ wtnt/user/user/urls.py
@@ -3,5 +3,7 @@ from .views import UserManageView, UserDeleteView
 
 urlpatterns = [
     path("manage", UserManageView.as_view()),
+    path("manage/<int:user_id>", UserManageView.as_view()),
     path("delete", UserDeleteView.as_view()),
+    path("delete/<int:user_id>", UserDeleteView.as_view()),
 ]
