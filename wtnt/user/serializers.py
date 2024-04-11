86ebc5d6060ae2488d40d9f07f8aca9472a15823
fnzksxl <fnzksxl@gmail.com>
2024-04-11 16:16:04 +0900

    #12 Refactor: Serializers 상속 관계 생성

--- wtnt/user/serializers.py
+++ wtnt/user/serializers.py
@@ -4,7 +4,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class ApproveUserSerializer(serializers.ModelSerializer):
+class BaseUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["name", "student_num", "id"]
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta:
+        model = User
+        fields = BaseUserSerializer.Meta.fields + ["image"]
