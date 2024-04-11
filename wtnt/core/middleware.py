d58f31efb6f100f4aa854eb4814b14f094c7c413
fnzksxl <fnzksxl@gmail.com>
2024-04-11 16:00:04 +0900

    #12 Feat: Middleware에서 refresh 관련 조작 기능 제거

--- wtnt/core/middleware.py
+++ wtnt/core/middleware.py
@@ -5,12 +5,12 @@ from rest_framework import status
 class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.APIS = ["github/login", "github/finish"]
+        self.API = "github/login"
         self.REFRESH = "token/refresh"
 
     def process_response(self, request, response):
         path = request.path_info
-        is_valid = any(api in path for api in self.APIS)
+        is_valid = True if self.API in path else False
         is_refresh = True if self.REFRESH in path else False
 
         if (
@@ -19,12 +19,10 @@ class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
             and (response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_201_CREATED)
         ):
             if request.META.get("HTTP_FROM", None) == "web":
-                response.set_cookie("refresh", response["refresh"])
                 response.set_cookie("access", response["access"])
 
                 if is_valid:
                     response.data.pop("access")
-                    response.data.pop("refresh")
 
                 response.content = response.render().rendered_content
 
@@ -34,19 +32,12 @@ class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
 class AttachJWTFromCookieToHeaderMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.NOT_APIS = ["github/login", "github/finish"]
-        self.REFRESH = "token/refresh"
+        self.NOT_API = "github/login"
 
     def process_request(self, request):
         path = request.path_info
-        is_valid = any(api in path for api in self.NOT_APIS)
-        is_refresh = True if self.REFRESH in path else False
+        is_valid = True if self.NOT_API in path else False
 
         if not is_valid:
             if request.META.get("HTTP_FROM", None) == "web":
-                request.META["access"] = request.COOKIES.get("access", None)
-                request.META["refresh"] = request.COOKIES.get("refresh", None)
-
-        if is_refresh:
-            if request.META.get("HTTP_FROM", None) == "web":
-                request.META["refresh"] = request.COOKIES.get("refresh", None)
+                request.META["HTTP_AUTHORIZATION"] = f"Bearer {request.COOKIES.get('access', None)}"
