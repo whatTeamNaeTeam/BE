faa4c8ba6c6ca2fcd712eaaf35d6c72cdf948ef4
fnzksxl <fnzksxl@gmail.com>
2024-04-11 16:16:32 +0900

    #12 Refactor: Serializer 명칭 변경

--- wtnt/user/user/views.py
+++ wtnt/user/user/views.py
@@ -2,13 +2,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
-from user.serializers import ApproveUserSerializer
+from user.serializers import BaseUserSerializer
 
 User = get_user_model()
 
 
 class UserManageView(APIView):
-    serializer_class = ApproveUserSerializer
+    serializer_class = BaseUserSerializer
     permission_classes = [IsAdminUser]
 
     def get(self, request):
