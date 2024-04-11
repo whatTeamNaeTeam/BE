1d2b832c69bd9c547a089c3e7f2ceaae3acae1e0
fnzksxl <fnzksxl@gmail.com>
2024-04-11 12:17:57 +0900

    #11 학번, 이름, 식별자만 파싱하는 시리얼라이저 작성

---
+++ wtnt/user/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ApproveUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["name", "student_num", "id"]
