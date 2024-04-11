6db5368f8f0de1de3f4190b1a78272548d1b3684
fnzksxl <fnzksxl@gmail.com>
2024-04-11 17:13:30 +0900

    #11 Feat: 회원 승인 기능 추가
    실패 시 400 상태 코드, 성공 시 202 상태 코드 반환

--- wtnt/user/user/views.py
+++ wtnt/user/user/views.py
@@ -1,18 +1,31 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
-from user.serializers import BaseUserSerializer
+
+from user.serializers import ApproveUserSerializer
 
 User = get_user_model()
 
 
 class UserManageView(APIView):
-    serializer_class = BaseUserSerializer
+    serializer_class = ApproveUserSerializer
     permission_classes = [IsAdminUser]
 
     def get(self, request):
-        queryset = User.objects.filter(is_apporoved=False)
+        queryset = User.objects.filter(is_approved=False)
         serializer = self.serializer_class(queryset, many=True)
 
         return Response(serializer.data)
+
+    def patch(self, request, *args, **kwargs):
+        user_id = request.data.get("id")
+        user = User.objects.get(id=user_id)
+
+        serializer = ApproveUserSerializer(user, data={"is_approved": True}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": True}, status=status.HTTP_202_ACCEPTED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
