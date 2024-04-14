bf9e581213f3e60fc181876b9f3f6c534adaad4e
fnzksxl <fnzksxl@gmail.com>
2024-04-14 23:22:31 +0900

    #16 Feat: Header 이름 변경으로 인한 미들웨어 수정

--- wtnt/core/middleware.py
+++ wtnt/core/middleware.py
@@ -5,12 +5,12 @@ from rest_framework import status
 class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.API = "github/login"
+        self.API = ["github/login", "callback/github"]
         self.REFRESH = "token/refresh"
 
     def process_response(self, request, response):
         path = request.path_info
-        is_valid = True if self.API in path else False
+        is_valid = any(path in api for api in self.API)
         is_refresh = True if self.REFRESH in path else False
 
         if (
@@ -18,7 +18,7 @@ class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
             or is_refresh
             and (response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_201_CREATED)
         ):
-            if request.META.get("HTTP_FROM", None) == "web":
+            if request.META.get("HTTP_X_FROM", None) == "web":
                 response.set_cookie("access", response["access"], httponly=True)
 
                 if is_valid:
@@ -32,12 +32,12 @@ class AttachJWTFromHeaderToCookieMiddleware(MiddlewareMixin):
 class AttachJWTFromCookieToHeaderMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
-        self.NOT_API = "github/login"
+        self.NOT_API = ["github/login", "callback/github"]
 
     def process_request(self, request):
         path = request.path_info
-        is_valid = True if self.NOT_API in path else False
+        is_valid = any(path in api for api in self.NOT_API)
 
         if not is_valid:
-            if request.META.get("HTTP_FROM", None) == "web":
+            if request.META.get("HTTP_X_FROM", None) == "web":
                 request.META["HTTP_AUTHORIZATION"] = f"Bearer {request.COOKIES.get('access', None)}"
