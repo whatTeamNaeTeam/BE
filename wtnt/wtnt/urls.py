c80085511074eba5d1f23cecbdcbd7e890b5aa1f
fnzksxl <fnzksxl@gmail.com>
2024-04-14 23:04:35 +0900

    #16 Feat: Swagger URL 등록

--- wtnt/wtnt/urls.py
+++ wtnt/wtnt/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WTNT PROJECT",
+        default_version="0.0.1",
+        description="WTNT API DOCS",
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("", include("allauth.urls")),
     path("api/", include("user.urls")),
     path("admin/", admin.site.urls),
