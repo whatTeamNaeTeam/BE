a9bb79f36fa4a6a9dcd13dc3fb83ee397cb48256
fnzksxl <fnzksxl@gmail.com>
2024-04-21 07:44:15 +0900

    #15 Feat: 팀 생성 뷰 작성

--- wtnt/team/views.py
+++ wtnt/team/views.py
@@ -1,3 +1,26 @@
-# from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+
+from core.permissions import IsApprovedUser
+from .serializers import TeamCreateSerializer
+from .utils import createSerializerHelper
 
 # Create your views here.
+
+User = get_user_model()
+
+
+class CreateTeamView(APIView):
+    permission_classes = [IsApprovedUser]
+
+    def post(self, request, *args, **kwargs):
+        url = createSerializerHelper.upload_s3(request.data.get("name"), request.FILES.get("image"))
+        team_data = createSerializerHelper.make_data(request.user.id, request.data, url)
+        createSerializer = TeamCreateSerializer(data=team_data)
+
+        if createSerializer.is_valid():
+            createSerializer.save()
+
+        return Response(createSerializer.data, status=status.HTTP_201_CREATED)
