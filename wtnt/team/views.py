acea61ec69f4b02aff64bd0c5facac7ec38e98ec
fnzksxl <fnzksxl@gmail.com>
2024-04-24 09:31:09 +0900

    #15 Feat: Team 생성 시 URL, 기술 스택이 함께 추가

--- wtnt/team/views.py
+++ wtnt/team/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 
 from core.permissions import IsApprovedUser
-from .serializers import TeamCreateSerializer
+from .serializers import TeamCreateSerializer, TeamUrlCreateSerializer, TeamTechCreateSerializer
 from .utils import createSerializerHelper
 
 # Create your views here.
@@ -23,4 +23,24 @@ class CreateTeamView(APIView):
         if createSerializer.is_valid():
             createSerializer.save()
 
-        return Response(createSerializer.data, status=status.HTTP_201_CREATED)
+            team_id = createSerializer.data.get("id")
+            team_urls = createSerializerHelper.make_urls_data(team_id, request.data.getlist("urls"))
+            urlSerializer = TeamUrlCreateSerializer(data=team_urls, many=True)
+
+            if urlSerializer.is_valid():
+                team_techs = createSerializerHelper.make_techs_data(
+                    team_id, request.data.getlist("subCategory"), request.data.getlist("memberCount")
+                )
+                techSerializer = TeamTechCreateSerializer(data=team_techs, many=True)
+
+                if techSerializer.is_valid():
+                    techSerializer.save()
+                    urlSerializer.save()
+
+                    response = createSerializerHelper.make_response(
+                        createSerializer.data, urlSerializer.data, techSerializer.data
+                    )
+
+                    return Response(response, status=status.HTTP_201_CREATED)
+
+        return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
