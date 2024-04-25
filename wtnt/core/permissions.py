e24ae470f7b854e583ffe3de9cf8a7ca6c3ea9de
fnzksxl <fnzksxl@gmail.com>
2024-04-26 00:08:34 +0900

    #19 Bug: Permission 수정

--- wtnt/core/permissions.py
+++ wtnt/core/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsApprovedUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_approved)
+        return bool(request.user.is_authenticated and request.user.is_approved)
