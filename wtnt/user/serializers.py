0fb8f6353950306c71b863f5c0d17cceda57831d
fnzksxl <fnzksxl@gmail.com>
2024-04-29 05:35:01 +0900

    #23 Feat: 기술 스택, URL 시리얼라이저 작성

--- wtnt/user/serializers.py
+++ wtnt/user/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from user.models import UserTech, UserUrls
+from core.fields import BinaryField
 
 User = get_user_model()
 
@@ -20,3 +22,21 @@ class UserProfileSerializer(UserSerializer):
     class Meta:
         model = User
         fields = UserSerializer.Meta.fields + ["is_approved", "is_staff", "position", "explain"]
+
+
+class UserUrlSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+    url = BinaryField()
+
+    class Meta:
+        model = UserUrls
+        fields = ["url", "user_id"]
+
+
+class UserTechSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+    tech = BinaryField()
+
+    class Meta:
+        model = UserTech
+        fields = ["tech", "user_id"]
