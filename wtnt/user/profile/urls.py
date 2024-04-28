73abdcf338a5bfa032d00a41d689c62f2cb09412
fnzksxl <fnzksxl@gmail.com>
2024-04-29 05:39:44 +0900

    #23 Feat: 기술스택, URL 등록 뷰 등록

--- wtnt/user/profile/urls.py
+++ wtnt/user/profile/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import UserProfileView, UserTechView, UserUrlView
 
 urlpatterns = [
     path("<int:user_id>", UserProfileView.as_view()),
+    path("tech/<int:user_id>", UserTechView.as_view()),
+    path("url/<int:user_id>", UserUrlView.as_view()),
 ]
