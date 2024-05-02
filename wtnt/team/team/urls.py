5d3737f1ddc59eb907405624e4792fbb1fd61bb3
fnzksxl <fnzksxl@gmail.com>
2024-05-03 08:17:08 +0900

    #27 Feat: 팀 상세 페이지 반환 URL 추가

--- wtnt/team/team/urls.py
+++ wtnt/team/team/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TeamView, TeamApplyView
+from .views import TeamView, TeamApplyView, TeamDetailView
 
 urlpatterns = [
     path("create", TeamView.as_view()),
     path("apply/<int:team_id>", TeamApplyView.as_view()),
+    path("detail/<int:team_id>", TeamDetailView.as_view()),
 ]
