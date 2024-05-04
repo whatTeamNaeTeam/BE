e44a51e607f7ae62bf8173196456ee51236637eb
fnzksxl <fnzksxl@gmail.com>
2024-05-04 14:28:46 +0900

    #27 Feat: 진행/완료 팀 리스트 API URL 추가

--- wtnt/team/team/urls.py
+++ wtnt/team/team/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TeamView, TeamApplyView, TeamDetailView
+from .views import TeamView, InProgressTeamView, TeamDetailView, AccomplishedTeamView
 
 urlpatterns = [
     path("create", TeamView.as_view()),
-    path("apply/<int:team_id>", TeamApplyView.as_view()),
+    path("inprogress", InProgressTeamView.as_view()),
+    path("accomplished", AccomplishedTeamView.as_view()),
     path("detail/<int:team_id>", TeamDetailView.as_view()),
 ]
