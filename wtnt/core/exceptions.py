297c24472538d15f6cc1fb8c241210e52f06c13d
fnzksxl <fnzksxl@gmail.com>
2024-05-07 23:56:28 +0900

    #31 Feat: 주인이 아닐 경우 Exception 추가

--- wtnt/core/exceptions.py
+++ wtnt/core/exceptions.py
@@ -4,3 +4,8 @@ from rest_framework.exceptions import APIException
 class IsNotLeaderException(APIException):
     status_code = 403
     default_detail = "It's not leader of team"
+
+
+class IsNotOwner(APIException):
+    status_code = 403
+    default_detail = "It's not an owner"
