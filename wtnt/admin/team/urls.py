16c4995632e24047644325320f041bdb17e258d3
fnzksxl <fnzksxl@gmail.com>
2024-05-08 00:51:52 +0900

    #32 Feat: URL 수정

--- wtnt/admin/team/urls.py
+++ wtnt/admin/team/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from .views import (
-    TeamManageGetListView,
-    TeamManageUpdateView,
+    TeamManageView,
     TeamDeleteView,
-    TeamGetListView,
 )
 
 urlpatterns = [
-    path("manage", TeamManageGetListView.as_view()),
-    path("manage/<int:team_id>", TeamManageUpdateView.as_view()),
-    path("list", TeamGetListView.as_view()),
-    path("list/<int:team_id>", TeamDeleteView.as_view()),
+    path("manage", TeamManageView.as_view()),
+    path("list", TeamDeleteView.as_view()),
 ]
