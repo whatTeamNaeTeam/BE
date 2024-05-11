9375f0d779b143f5a2a3fd0df949b4e3a531c732
fnzksxl <fnzksxl@gmail.com>
2024-05-12 00:47:17 +0900

    #32 Feat: 팀 검색 URL 추가

--- wtnt/admin/team/urls.py
+++ wtnt/admin/team/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     TeamManageView,
     TeamDeleteView,
+    TeamSearchView,
 )
 
 urlpatterns = [
     path("manage", TeamManageView.as_view()),
     path("list", TeamDeleteView.as_view()),
+    path("search", TeamSearchView.as_view()),
 ]
