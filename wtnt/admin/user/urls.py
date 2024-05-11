4703fdf968f5fb8807db3241ff336690da806283
fnzksxl <fnzksxl@gmail.com>
2024-05-11 22:10:27 +0900

    #32 Feat: View 간소화에 따른 URL 수정

--- wtnt/admin/user/urls.py
+++ wtnt/admin/user/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from .views import (
-    UserManageGetListView,
-    UserManageUpdateView,
+    UserManageView,
     UserDeleteView,
-    UserGetListView,
 )
 
 urlpatterns = [
-    path("manage", UserManageGetListView.as_view()),
-    path("manage/<int:user_id>", UserManageUpdateView.as_view()),
-    path("list", UserGetListView.as_view()),
-    path("list/<int:user_id>", UserDeleteView.as_view()),
+    path("manage", UserManageView.as_view()),
+    path("list", UserDeleteView.as_view()),
 ]
