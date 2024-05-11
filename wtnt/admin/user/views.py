bb61d2d44c700075ffcf67e3148c057337cd3383
fnzksxl <fnzksxl@gmail.com>
2024-05-12 00:22:47 +0900

    #32 Feat: 유저 이름/학번/포지션 별 검색 기능 추가

--- wtnt/admin/user/views.py
+++ wtnt/admin/user/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from core.pagenations import ListPagenationSize10
 from admin.serializers import ApproveUserSerializer
@@ -59,6 +60,35 @@ class UserDeleteView(APIView, ListPagenationSize10):
             user.delete()
 
             return Response({"success": True}, status=status.HTTP_204_NO_CONTENT)
+
+        except User.DoesNotExist:
+            return Response({"error": "User Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UserSearchView(APIView, ListPagenationSize10):
+    permission_classes = [IsAdminUser]
+    serializer_class = ApproveUserSerializer
+
+    def get(self, request, *args, **kwargs):
+        keyword = request.query_params.get("keyword")
+        search_filter = request.query_params.get("filter")
+
+        try:
+            if search_filter == "name":
+                queryset = User.objects.filter(Q(name__icontains=keyword), is_approved=True).order_by("student_num")
+            elif search_filter == "student_num":
+                queryset = User.objects.filter(Q(student_num__icontains=keyword), is_approved=True).order_by(
+                    "student_num"
+                )
+            elif search_filter == "position":
+                queryset = User.objects.filter(Q(position__icontains=keyword), is_approved=True).order_by("student_num")
+            else:
+                return Response({"error": "No Filter"}, status=status.HTTP_400_BAD_REQUEST)
+
+            paginated = self.paginate_queryset(queryset, request, view=self)
+            serializer = self.serializer_class(paginated, many=True)
+
+            return self.get_paginated_response(serializer.data)
 
         except User.DoesNotExist:
             return Response({"error": "User Not Found"}, status=status.HTTP_404_NOT_FOUND)
