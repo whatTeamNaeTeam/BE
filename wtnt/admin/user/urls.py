a0de3c12e4f62c21c59f382207093f63d993ea4c
fnzksxl <fnzksxl@gmail.com>
2024-05-12 00:23:04 +0900

    #32 Feat: 유저 검색 URL 추가

--- wtnt/admin/user/urls.py
+++ wtnt/admin/user/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     UserManageView,
     UserDeleteView,
+    UserSearchView,
 )
 
 urlpatterns = [
     path("manage", UserManageView.as_view()),
     path("list", UserDeleteView.as_view()),
+    path("search", UserSearchView.as_view()),
 ]
