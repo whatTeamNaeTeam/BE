da81d465796fde6d7df16775c57d8e0af80c520d
fnzksxl <fnzksxl@gmail.com>
2024-05-12 00:46:56 +0900

    #32 Feat: 팀 검색 기능 추가

--- wtnt/admin/team/views.py
+++ wtnt/admin/team/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from core.pagenations import ListPagenationSize10
 from team.models import Team
@@ -61,3 +62,31 @@ class TeamDeleteView(APIView, ListPagenationSize10):
 
         except User.DoesNotExist:
             return Response({"error": "Team Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class TeamSearchView(APIView, ListPagenationSize10):
+    permission_classes = [IsAdminUser]
+    serializer_class = ApproveTeamSerializer
+
+    def get(self, request):
+        keyword = request.query_params.get("keyword")
+        search_filter = request.query_params.get("filter")
+
+        try:
+            if search_filter == "name":
+                queryset = Team.objects.filter(Q(name__icontains=keyword), is_approved=True).order_by("id")
+            elif search_filter == "genre":
+                queryset = Team.objects.filter(Q(genre__icontains=keyword), is_approved=True).order_by("id")
+            elif search_filter == "leader":
+                leader = User.objects.get(name=keyword)
+                queryset = Team.objects.filter(leader_id=leader.id, is_approved=True).order_by("id")
+            else:
+                return Response({"error": "No Filter"}, status=status.HTTP_400_BAD_REQUEST)
+
+            paginated = self.paginate_queryset(queryset, request, view=self)
+            serializer = self.serializer_class(paginated, many=True)
+
+            return self.get_paginated_response(serializer.data)
+
+        except Team.DoesNotExist:
+            return Response({"error": "User Not Found"}, status=status.HTTP_404_NOT_FOUND)
