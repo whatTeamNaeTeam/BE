10d55feee44d243b944e07107e3b92a9ecac881b
fnzksxl <fnzksxl@gmail.com>
2024-05-14 15:32:34 +0900

    #35 Feat: 뷰 통합에 따라 url 수정

--- wtnt/team/team/urls.py
+++ wtnt/team/team/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import TeamView, InProgressTeamView, TeamDetailView, AccomplishedTeamView
+from .views import TeamView, TeamListView, TeamDetailView
 
 urlpatterns = [
     path("create", TeamView.as_view()),
-    path("inprogress", InProgressTeamView.as_view()),
-    path("accomplished", AccomplishedTeamView.as_view()),
+    path("list", TeamListView.as_view()),
     path("detail/<int:team_id>", TeamDetailView.as_view()),
 ]
