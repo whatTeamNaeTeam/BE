0f6d4531861567f54cca142afb86465634abc0ed
fnzksxl <fnzksxl@gmail.com>
2024-05-16 20:31:59 +0900

    #38 Minor: URL, TECH 없을 시에 반환하는 Default값 수정

--- wtnt/user/utils.py
+++ wtnt/user/utils.py
@@ -2,8 +2,8 @@ class ProfileSerializerHelper:
     def make_data(self, owner_id, profile, url, tech):
         return {
             "profile": profile.data,
-            "urls": self.make_url_data(url.data["url"]) if url else None,
-            "tech": self.make_tech_data(tech.data["tech"]) if tech else None,
+            "urls": self.make_url_data(url.data["url"]) if url else [],
+            "tech": self.make_tech_data(tech.data["tech"]) if tech else [],
             "is_owner": True if owner_id == profile.data["id"] else False,
         }
 
