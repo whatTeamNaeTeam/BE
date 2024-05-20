bbea3d6261679239e0a77f233c489cf3641a2aa3
fnzksxl <fnzksxl@gmail.com>
2024-05-21 05:34:27 +0900

    #38 Feat: 새로운 뷰 URL추가

--- wtnt/user/auth/urls.py
+++ wtnt/user/auth/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import GithubLoginView, GithubOAuthCallBackView, FinishGithubLoginView, WtntTokenRefreshView
+from .views import (
+    GithubLoginView,
+    GithubOAuthCallBackView,
+    FinishGithubLoginView,
+    WtntTokenRefreshView,
+    EmailVerifyView,
+)
 
 urlpatterns = [
     path("github/callback", GithubOAuthCallBackView.as_view()),
     path("github/login", GithubLoginView.as_view()),
     path("github/finish", FinishGithubLoginView.as_view()),
     path("token/refresh", WtntTokenRefreshView.as_view()),
+    path("email", EmailVerifyView.as_view()),
 ]
