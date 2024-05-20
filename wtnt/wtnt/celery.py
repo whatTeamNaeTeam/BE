252e33e867d6084b1ad04ec3aceca1ce22a5e7b7
fnzksxl <fnzksxl@gmail.com>
2024-05-21 05:17:03 +0900

    #38 Feat: Celery Beat로 매일 자정 Task가 실행되도록 설정

--- wtnt/wtnt/celery.py
+++ wtnt/wtnt/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wtnt.settings")
 os.environ.setdefault("FORKED_BY_MULTIPROCESSING", "1")  # WINDOWS에서 돌리기 위한 설정
@@ -8,6 +9,10 @@ app = Celery("wtnt")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "delete-view-history-per-day": {"task": "team.tasks.delete_view_history", "schedule": crontab(minute=0, hour=0)}
+}
 
 
 @app.task(bind=True)
