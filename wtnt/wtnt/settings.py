57465747fea50a26d04e0c7c8fed5a7e0efc6a77
fnzksxl <fnzksxl@gmail.com>
2024-05-21 05:30:38 +0900

    #38 Feat: 이메일 서버 세팅 추가

--- wtnt/wtnt/settings.py
+++ wtnt/wtnt/settings.py
@@ -230,3 +230,13 @@ CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 # CELERY_RESULT_SERIALIZER = 'json'
 # CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = "Asia/Seoul"
+
+# Email Settings
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_ID")
+EMAIL_HOST_PASSWORD = env("EMAIL_PW")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
