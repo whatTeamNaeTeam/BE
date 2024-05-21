c458b94db1b85a04c91a7051e9b24e8fc4aec81d
fnzksxl <fnzksxl@gmail.com>
2024-05-22 08:55:52 +0900

    #40 Feat: 활동 내역에서 진행/완료/가입 신청 프로젝트 각각 확인할 수 있도록 API 구성

--- wtnt/user/profile/views.py
+++ wtnt/user/profile/views.py
@@ -5,8 +5,11 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 
 from user.serializers import UserProfileSerializer, UserTechSerializer, UserUrlSerializer
+from team.serializers import TeamListSerializer
 from user.models import UserTech, UserUrls
+from team.models import TeamApply, Team, TeamUser
 from user.utils import profileSerializerHelper
+from team.utils import createSerializerHelper
 from core.exceptions import IsNotOwner
 
 User = get_user_model()
@@ -140,3 +143,28 @@ class UserUrlView(APIView):
 
         else:
             return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserMyActivityView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        user_id = kwargs.get("user_id")
+        keyword = request.query_params.get("keyword")
+        if keyword == "apply":
+            team_ids = TeamApply.objects.filter(user_id=user_id, is_approved=False).values_list("team_id", flat=True)
+            team_data = Team.objects.filter(id__in=team_ids)
+        else:
+            team_ids = TeamUser.objects.filter(user_id=user_id).values_list("team_id", flat=True)
+            if keyword == "accomplished":
+                team_data = Team.objects.filter(id__in=team_ids, is_accomplished=True, is_approved=True)
+            elif keyword == "inprogress":
+                team_data = Team.objects.filter(id__in=team_ids, is_accomplished=False, is_approved=True)
+            else:
+                return Response({"error": "Wrong Keyword"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = TeamListSerializer(team_data, many=True)
+        data = createSerializerHelper.make_responses(serializer.data, request.user.id)
+        is_owner = True if user_id == request.user.id else False
+
+        return Response({"team": data, "is_owner": is_owner}, status=status.HTTP_200_OK)
