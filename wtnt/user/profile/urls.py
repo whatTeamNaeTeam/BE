1eb4263f6618bc06ef3ad0601d6dfe6b6c5d0223
fnzksxl <fnzksxl@gmail.com>
2024-05-22 08:56:08 +0900

    #40 Feat: 활동 내역 확인 URL추가

--- wtnt/user/profile/urls.py
+++ wtnt/user/profile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserProfileView, UserTechView, UserUrlView
+from .views import UserProfileView, UserTechView, UserUrlView, UserMyActivityView
 
 urlpatterns = [
     path("<int:user_id>", UserProfileView.as_view()),
     path("tech/<int:user_id>", UserTechView.as_view()),
     path("url/<int:user_id>", UserUrlView.as_view()),
+    path("activity/<int:user_id>", UserMyActivityView.as_view()),
 ]
