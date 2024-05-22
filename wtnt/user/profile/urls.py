f79ce85578a8d5b666adb0e26ac151d5dc190643
fnzksxl <fnzksxl@gmail.com>
2024-05-22 09:32:08 +0900

    #40 Feat: 팀 관리 탭 URL 추가

--- wtnt/user/profile/urls.py
+++ wtnt/user/profile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserProfileView, UserTechView, UserUrlView, UserMyActivityView
+from .views import UserProfileView, UserTechView, UserUrlView, UserMyActivityView, UserManageActivityView
 
 urlpatterns = [
     path("<int:user_id>", UserProfileView.as_view()),
     path("tech/<int:user_id>", UserTechView.as_view()),
     path("url/<int:user_id>", UserUrlView.as_view()),
     path("activity/<int:user_id>", UserMyActivityView.as_view()),
+    path("team-manage/<int:user_id>", UserManageActivityView.as_view()),
 ]
