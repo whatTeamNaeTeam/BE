4a4d8653e481b7671a6cd28b1644b3cc2bd0672f
fnzksxl <fnzksxl@gmail.com>
2024-05-28 03:17:19 +0900

    #47 Refactor: Redis 관련 함수 Class로 묶기

--- wtnt/user/auth/utils.py
+++ wtnt/user/auth/utils.py
@@ -5,25 +5,27 @@ from datetime import datetime, timedelta
 client = get_redis_connection()
 
 
-def set_refresh_token_in_cache(user_id, refresh_token):
-    cache.set(user_id, refresh_token)
-    cache.expire_at(user_id, datetime.now() + timedelta(days=7))
+class RedisAuthUtils:
+    @staticmethod
+    def set_refresh_token_in_cache(user_id, refresh_token):
+        cache.set(user_id, refresh_token)
+        cache.expire_at(user_id, datetime.now() + timedelta(days=7))
 
+    @staticmethod
+    def get_refresh_token_in_cache(user_id):
+        return cache.get(user_id)
 
-def get_refresh_token_in_cache(user_id):
-    return cache.get(user_id)
+    @staticmethod
+    def set_code_in_redis_from_email(email, code):
+        client.set(email, code)
 
+    @staticmethod
+    def get_code_in_redis_from_email(email):
+        return client.get(email).decode()
 
-def set_code_in_redis_from_email(email, code):
-    client.set(email, code)
-
-
-def get_code_in_redis_from_email(email):
-    return client.get(email).decode()
-
-
-def delete_code_in_redis_from_email(email):
-    client.delete(email)
+    @staticmethod
+    def delete_code_in_redis_from_email(email):
+        client.delete(email)
 
 
 def get_user_info(user):
