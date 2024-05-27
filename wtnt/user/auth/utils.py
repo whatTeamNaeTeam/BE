237f9545084882f55238f480983d52786aa076b5
fnzksxl <fnzksxl@gmail.com>
2024-05-28 02:44:19 +0900

    #47 Refactor: Refresh Token 획득 함수 추가

--- wtnt/user/auth/utils.py
+++ wtnt/user/auth/utils.py
@@ -7,6 +7,10 @@ def set_refresh_token_in_cache(user_id, refresh_token):
     cache.expire_at(user_id, datetime.now() + timedelta(days=7))
 
 
+def get_refresh_token_in_cache(user_id):
+    return cache.get(user_id)
+
+
 def get_user_info(user):
     return {
         "name": user.name,
