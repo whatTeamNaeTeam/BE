b99a70c3842557d7a128f058905e428f85fa5d7c
fnzksxl <fnzksxl@gmail.com>
2024-05-28 01:10:21 +0900

    #47 Refactor: 자잘한 함수 Utils 함수로 빼내기

---
+++ wtnt/user/auth/utils.py
@@ -0,0 +1,16 @@
+from django.core.cache import cache
+from datetime import datetime, timedelta
+
+
+def set_refresh_token_in_cache(user_id, refresh_token):
+    cache.set(user_id, refresh_token)
+    cache.expire_at(user_id, datetime.now() + timedelta(days=7))
+
+
+def get_user_info(user):
+    return {
+        "name": user.name,
+        "student_num": user.student_num,
+        "id": user.id,
+        "image": user.image,
+    }
