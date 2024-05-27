a002bb3b90b84193b6f719cf3dedb5a5ee45aefe
fnzksxl <fnzksxl@gmail.com>
2024-05-28 01:56:13 +0900

    #47 Refactor: 회원가입 비즈니스 로직 Service 단으로 추출

--- wtnt/user/auth/service.py
+++ wtnt/user/auth/service.py
@@ -1,7 +1,12 @@
+from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth import get_user_model
+from django_redis import get_redis_connection
+
 from .utils import set_refresh_token_in_cache, get_user_info
+from core.exceptions import CodeNotMatchError
 
 User = get_user_model()
+client = get_redis_connection()
 
 
 class AuthService:
@@ -38,3 +43,22 @@ class AuthService:
 
         response_data.pop("access")
         return response_data, access_token
+
+
+class RegisterService:
+    def __init__(self, request):
+        self.request = request
+
+    def finish_register_by_user_input(self):
+        code = self.request.data.get("code")
+        email = self.request.data.get("email")
+        if code != client.get(email).decode():
+            raise CodeNotMatchError()
+
+        extra_data = SocialAccount.objects.get(user_id=self.request.user.id).extra_data
+        user = User.objects.get(id=self.request.user.id)
+        user.finish_register(extra_data=extra_data, request_data=self.request.data)
+
+        client.delete(email)
+
+        return user
