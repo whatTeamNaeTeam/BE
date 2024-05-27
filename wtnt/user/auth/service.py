01ddd14a64d4f8cea859d623ba04b81b25a705f5
fnzksxl <fnzksxl@gmail.com>
2024-05-28 02:44:50 +0900

    #47 Refactor: 토큰 리프레싱 비즈니스 로직 추출

--- wtnt/user/auth/service.py
+++ wtnt/user/auth/service.py
@@ -1,9 +1,11 @@
 from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth import get_user_model
 from django_redis import get_redis_connection
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
-from .utils import set_refresh_token_in_cache, get_user_info
-from core.exceptions import CodeNotMatchError
+from .utils import set_refresh_token_in_cache, get_refresh_token_in_cache, get_user_info
+from core.exceptions import CodeNotMatchError, RefreshTokenExpired
 
 User = get_user_model()
 client = get_redis_connection()
@@ -62,3 +64,28 @@ class RegisterService:
         client.delete(email)
 
         return user
+
+
+class RefreshService:
+    def __init__(self, request):
+        self.request = request
+
+    def extract_refresh_token(self):
+        _, access_token = self.request.META.get("HTTP_AUTHORIZATION").split(" ")
+        user_id = AccessToken(access_token, verify=False).payload.get("user_id")
+
+        refresh_token = get_refresh_token_in_cache(user_id)
+        if not refresh_token:
+            raise RefreshTokenExpired()
+
+        return refresh_token
+
+    def get_new_access_token_from_serializer(self, serializer):
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        token = serializer.validated_data
+
+        return token
