d3d727b248a7f4952c46f96e39939e6a61095c83
fnzksxl <fnzksxl@gmail.com>
2024-05-28 14:50:19 +0900

    #49 Refactor: Tech Response 생성 함수 추가

--- wtnt/user/profile/utils.py
+++ wtnt/user/profile/utils.py
@@ -5,3 +5,8 @@ def make_data(user, url, tech, user_id):
 def make_url_data(url):
     data = url.split(",")
     return [{"url": url} for url in data]
+
+
+def make_tech_data(tech):
+    data = tech.split(",")
+    return [{"name": name} for name in data]
