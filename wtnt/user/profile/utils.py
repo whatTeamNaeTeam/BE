3c46abe8e3b3b8b2add333b54e303103c374e3e3
fnzksxl <fnzksxl@gmail.com>
2024-05-28 14:42:44 +0900

    #49 Refactor: URL Response 생성 함수 추가

--- wtnt/user/profile/utils.py
+++ wtnt/user/profile/utils.py
@@ -1,2 +1,7 @@
 def make_data(user, url, tech, user_id):
     return {"profile": user, "url": url, "tech": tech, "is_owner": user["id"] == user_id}
+
+
+def make_url_data(url):
+    data = url.split(",")
+    return [{"url": url} for url in data]
