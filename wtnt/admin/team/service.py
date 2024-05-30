c95e556296bbfbc00ab7e96173a00a8b6c45d028
fnzksxl <fnzksxl@gmail.com>
2024-05-30 15:55:03 +0900

    #51 Refactor: 팀 검색 비즈니스 로직 추출

--- wtnt/admin/team/service.py
+++ wtnt/admin/team/service.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
+
 from admin.serializers import ApproveTeamSerializer
-from core.exceptions import NotFoundError
+from core.exceptions import NotFoundError, KeywordNotMatchError
 from core.pagenations import ListPagenationSize10
 from core.service import BaseService
 from team.models import Team
+
+User = get_user_model()
 
 
 class AdminTeamService(BaseService, ListPagenationSize10):
@@ -33,6 +37,28 @@ class AdminTeamService(BaseService, ListPagenationSize10):
 
     def get_approved_teams(self):
         queryset = Team.objects.filter(is_approved=True).order_by("id")
+        paginated = self.paginate_queryset(queryset, self.request, view=self)
+        serializer = ApproveTeamSerializer(paginated, many=True)
+
+        return self.get_paginated_response(serializer.data)
+
+    def serach_teams(self):
+        keyword = self.request.query_params.get("keyword")
+        search_filter = self.request.query_params.get("filter")
+
+        if search_filter == "name":
+            queryset = Team.objects.search_by_name(name=keyword)
+        elif search_filter == "genre":
+            queryset = Team.objects.search_by_genre(genre=keyword)
+        elif search_filter == "leader":
+            leader_ids = User.objects.search_by_name(name=keyword).values_list("id", flat=True)
+            queryset = Team.objects.search_by_leader_ids(leader_ids=leader_ids)
+        else:
+            raise KeywordNotMatchError
+
+        if not queryset:
+            raise NotFoundError()
+
         paginated = self.paginate_queryset(queryset, self.request, view=self)
         serializer = ApproveTeamSerializer(paginated, many=True)
 
