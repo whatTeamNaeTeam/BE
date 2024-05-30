933fd37ab9fa1802b503a8de7e9e5518b44af533
fnzksxl <fnzksxl@gmail.com>
2024-05-30 13:04:54 +0900

    #51 Refactor: 승인된 유저 관리 비즈니스 로직 추출

--- wtnt/admin/user/service.py
+++ wtnt/admin/user/service.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
 
 from core.exceptions import NotFoundError
+from core.pagenations import ListPagenationSize10
 from core.service import BaseService
 from admin.serializers import ApproveUserSerializer
 
 User = get_user_model()
 
 
-class AdminUserService(BaseService):
+class AdminUserService(BaseService, ListPagenationSize10):
     def get_not_approved_users(self):
         try:
             queryset = User.objects.filter(is_approved=False, is_superuser=False)
@@ -25,10 +26,17 @@ class AdminUserService(BaseService):
         else:
             raise NotFoundError()
 
-    def reject_users(self):
+    def reject_users(self, status):
         user_ids = [int(id) for id in self.request.data.get("ids").split(",")]
-        cnt, _ = User.objects.filter(id__in=user_ids).delete()
+        cnt, _ = User.objects.filter(id__in=user_ids, is_approved=status).delete()
         if cnt:
             return {"detail": "Success to reject users"}
         else:
             raise NotFoundError()
+
+    def get_approved_users(self):
+        queryset = User.objects.filter(is_approved=True, is_superuser=False).order_by("student_num")
+        paginated = self.paginate_queryset(queryset, self.request, view=self)
+        serializer = ApproveUserSerializer(paginated, many=True)
+
+        return self.get_paginated_response(serializer.data)
