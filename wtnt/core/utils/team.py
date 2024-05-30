3cc9b2cf47294ac652627a9c7c8aaf93af0be9c9
fnzksxl <fnzksxl@gmail.com>
2024-05-30 18:14:14 +0900

    #53 Refactor: 팀 생성 util 함수 추출

--- wtnt/core/utils/team.py
+++ wtnt/core/utils/team.py
@@ -1,4 +1,7 @@
+import boto3
+
 from team.models import Likes
+import wtnt.settings as settings
 
 
 def make_team_list(team_data, user_id, is_manage=None):
@@ -29,3 +32,36 @@ def is_likes(team_ids, user_id):
         result[idx] = likes_dict.get(team_id, False)
 
     return result
+
+
+def make_data(leader, strs, image, categories, counts):
+    _dict = {
+        "name": strs.get("name"),
+        "leader_id": leader,
+        "explain": strs.get("explain"),
+        "genre": strs.get("genre"),
+        "image": image,
+        "url": strs.get("urls", "No"),
+        "category": make_tech_data(categories, counts),
+    }
+
+    return _dict
+
+
+def make_tech_data(categories, counts):
+    return [{"tech": category, "need_num": count} for category, count in zip(categories, counts)]
+
+
+class S3Utils:
+    @staticmethod
+    def upload_s3(name, image):
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=settings.AWS_CLIENT_ID,
+            aws_secret_access_key=settings.AWS_CLIENT_SECRET,
+            region_name=settings.AWS_REGION,
+        )
+        root = "team/" + name + "/image.jpg"
+        s3_client.upload_fileobj(image, settings.BUCKET_NAME, root)
+
+        return f"https://{settings.BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{root}"
