3ce5885fd52c8919dbb03de3cd51011784c59b84
fnzksxl <fnzksxl@gmail.com>
2024-05-31 16:04:59 +0900

    #53 Refactor: 함수 호출로 인한 뷰 간소화

--- wtnt/team/team/views.py
+++ wtnt/team/team/views.py
@@ -5,10 +5,9 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from django_redis import get_redis_connection
 
-from core.exceptions import IsNotLeaderError
 from core.permissions import IsApprovedUser
 from core.pagenations import TeamPagination
-from team.serializers import TeamCreateSerializer, TeamListSerializer
+from team.serializers import TeamListSerializer
 from team.utils import createSerializerHelper
 from team.models import Team
 from .service import TeamService
@@ -31,58 +30,16 @@ class TeamDetailView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
-        team_id = kwargs.get("team_id")
+        team_service = TeamService(request, **kwargs)
+        data = team_service.get_team_detail()
 
-        if not request.user.id:
-            user_id = 0
-        else:
-            user_id = request.user.id
-        try:
-            redis_ans = client.sadd(f"views:{team_id}", f"{user_id}_{request.META.get('REMOTE_ADDR')}")
-            team = Team.objects.get(id=team_id)
-            if redis_ans:
-                team.view += 1
-                team.save()
-            teamSerializer = TeamCreateSerializer(team)
-            response = createSerializerHelper.make_response(teamSerializer.data, request.user.id)
-
-            return Response(response, status=status.HTTP_200_OK)
-
-        except Team.DoesNotExist:
-            return Response({"error": "No Content"}, status=status.HTTP_404_NOT_FOUND)
-
-    def patch(self, request, *args, **kwargs):
-        team_id = kwargs.get("team_id")
-        try:
-            team = Team.objects.get(id=team_id)
-            if team.leader != request.user.id:
-                raise IsNotLeaderError()
-
-            url = request.data.get("urls")
-            serializer = TeamCreateSerializer(team, {"url": url}, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({"urls": url.split(",")}, status=status.HTTP_202_ACCEPTED)
-
-        except Team.DoesNotExist:
-            return Response({"error": "No Content"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(data, status=status.HTTP_200_OK)
 
     def put(self, request, *args, **kwargs):
-        team_id = kwargs.get("team_id")
-        try:
-            team = Team.objects.get(id=team_id)
-            if team.leader != request.user.id:
-                raise IsNotLeaderError()
+        team_service = TeamService(request, **kwargs)
+        data = team_service.update_team()
 
-            name = request.data.get("name")
-            explain = request.data.get("explain")
-            serializer = TeamCreateSerializer(team, data={"name": name, "explain": explain}, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({"name": name, "explain": explain}, status=status.HTTP_202_ACCEPTED)
-
-        except Team.DoesNotExist:
-            return Response({"error": "No Content"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(data, status=status.HTTP_202_ACCEPTED)
 
 
 class TeamListView(APIView, TeamPagination):
