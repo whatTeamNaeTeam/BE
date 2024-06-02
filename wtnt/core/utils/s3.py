94211d5d311b837425de72a2400266d8f383399e
fnzksxl <fnzksxl@gmail.com>
2024-06-02 11:54:14 +0900

    #57 Refactor: 확장성을 위해 클래스메소드로 변경

--- wtnt/core/utils/s3.py
+++ wtnt/core/utils/s3.py
@@ -4,15 +4,23 @@ import wtnt.settings as settings
 
 
 class S3Utils:
-    @staticmethod
-    def upload_team_image_on_s3(name, image):
-        s3_client = boto3.client(
-            "s3",
-            aws_access_key_id=settings.AWS_CLIENT_ID,
-            aws_secret_access_key=settings.AWS_CLIENT_SECRET,
-            region_name=settings.AWS_REGION,
-        )
-        root = "team/" + name + "/image.jpg"
-        s3_client.upload_fileobj(image, settings.BUCKET_NAME, root)
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_CLIENT_ID,
+        aws_secret_access_key=settings.AWS_CLIENT_SECRET,
+        region_name=settings.AWS_REGION,
+    )
+    bucket = settings.BUCKET_NAME
+    region = settings.AWS_REGION
 
-        return f"https://{settings.BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{root}"
+    @staticmethod
+    def get_team_image_name(name):
+        return "team/" + name + "/image.jpg"
+
+    @classmethod
+    def upload_team_image_on_s3(cls, name, image):
+        s3_client = cls.client
+        root = cls.get_team_image_name(name)
+        s3_client.upload_fileobj(image, cls.bucket, root)
+
+        return f"https://{cls.bucket}.s3.{cls.region}.amazonaws.com/{root}"
