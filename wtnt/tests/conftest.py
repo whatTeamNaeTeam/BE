f84a7807347ae728d2c8a27350a26940250affe5
fnzksxl <fnzksxl@gmail.com>
2024-06-19 08:53:04 +0900

    #59 Test: Github SocialApp Fixture 추가

--- wtnt/tests/conftest.py
+++ wtnt/tests/conftest.py
@@ -1,7 +1,20 @@
 import pytest
+
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
 from rest_framework.test import APIClient
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def social_app(db):
+    site = Site.objects.get_current()
+    social_app = SocialApp.objects.create(
+        provider="github", name="GitHub", client_id="fake-client-id", secret="fake-client-secret"
+    )
+    social_app.sites.add(site)
+    return social_app
