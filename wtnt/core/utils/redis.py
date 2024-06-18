1156eb77e6ed42c2046952e2f6aaa80d09624d03
fnzksxl <fnzksxl@gmail.com>
2024-06-19 06:23:30 +0900

    #59 Feat: Test 시 Redis 모킹을 위해 클래스 구조 변경

--- wtnt/core/utils/redis.py
+++ wtnt/core/utils/redis.py
@@ -3,31 +3,40 @@ from datetime import datetime, timedelta
 from django.core.cache import cache
 from django_redis import get_redis_connection
 
-client = get_redis_connection()
-
 
 class RedisUtils:
-    @staticmethod
-    def sadd_view_client(team_id, user_id, adress):
-        return client.sadd(f"views:{team_id}", f"{user_id}_{adress}")
+    client = None
 
-    @staticmethod
-    def set_refresh_token_in_cache(user_id, refresh_token):
+    @classmethod
+    def init(cls):
+        if not cls.client:
+            cls.client = get_redis_connection()
+
+    @classmethod
+    def sadd_view_client(cls, team_id, user_id, adress):
+        RedisUtils.init()
+        return cls.client.sadd(f"views:{team_id}", f"{user_id}_{adress}")
+
+    @classmethod
+    def set_refresh_token_in_cache(cls, user_id, refresh_token):
         cache.set(user_id, refresh_token)
         cache.expire_at(user_id, datetime.now() + timedelta(days=7))
 
-    @staticmethod
-    def get_refresh_token_in_cache(user_id):
+    @classmethod
+    def get_refresh_token_in_cache(cls, user_id):
         return cache.get(user_id)
 
-    @staticmethod
-    def set_code_in_redis_from_email(email, code):
-        client.set(email, code)
+    @classmethod
+    def set_code_in_redis_from_email(cls, email, code):
+        RedisUtils.init()
+        cls.client.set(email, code)
 
-    @staticmethod
-    def get_code_in_redis_from_email(email):
-        return client.get(email).decode()
+    @classmethod
+    def get_code_in_redis_from_email(cls, email):
+        RedisUtils.init()
+        return cls.client.get(email).decode()
 
-    @staticmethod
-    def delete_code_in_redis_from_email(email):
-        client.delete(email)
+    @classmethod
+    def delete_code_in_redis_from_email(cls, email):
+        RedisUtils.init()
+        cls.client.delete(email)
