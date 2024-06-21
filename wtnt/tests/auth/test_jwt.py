4b7711f80f5945ea10b13ac6b680347b2b6a0993
fnzksxl <fnzksxl@gmail.com>
2024-06-21 09:51:10 +0900

    #59 Test: AccessToken 유무 및 이로 인한 권한 체크 테스트 케이스 추가

--- wtnt/tests/auth/test_jwt.py
+++ wtnt/tests/auth/test_jwt.py
@@ -29,3 +29,22 @@ class TestJWT:
         response = self.api_client.post(url, {}, format="json", headers=headers)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_authencation_failed(self, registered_user):
+        user_id = registered_user.id
+
+        url = reverse("profile-team-manage", args=[user_id])  # Random View Need Authentication
+
+        response = self.api_client.get(url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_authentication_failed_by_permission(self, registered_user, access_token):
+        user_id = registered_user.id
+
+        headers = {"Authorization": f"Bearer {str(access_token)}", "Content-Type": "application/json"}
+        url = reverse("profile-team-manage", args=[user_id])
+
+        response = self.api_client.get(url, headers=headers)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
