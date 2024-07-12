d3a19f5e6bb6841636334ace368ee08c28123d2c
fnzksxl <fnzksxl@gmail.com>
2024-07-12 18:45:17 +0900

    #63 Bug: 배포 서버에서도 next 링크가 localhost:8000으로 표기되는 것 개선

--- wtnt/wtnt/settings.py
+++ wtnt/wtnt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
-
+MY_DOMAIN = "https://api.whatmeow.shop"
 SITE_ID = 1
 
 ALLOWED_HOSTS = ["*"]
