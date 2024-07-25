f847b18d5531e10659fd80de1c38902317e87b41
fnzksxl <fnzksxl@gmail.com>
2024-07-26 08:02:12 +0900

    #66 Feat: TeamManageDetail 최종 응답 데이터 헬퍼 함수 작성

--- wtnt/core/utils/profile.py
+++ wtnt/core/utils/profile.py
@@ -18,3 +18,20 @@ class ProfileResponse:
     @staticmethod
     def make_activity_data(teams, user_id, owner_id):
         return {"team": teams, "is_owner": user_id == owner_id}
+
+    @staticmethod
+    def make_team_manage_detail_data(members, team):
+        leader_id = team.leader.id
+
+        for i, member in enumerate(members):
+            if member["id"] == leader_id:
+                leader_info = members.pop(i)
+                break
+
+        return {
+            "title": team.title,
+            "leader_info": leader_info,
+            "members_info": members,
+            "member_count": len(members) + 1,
+            "team_id": team.id,
+        }
