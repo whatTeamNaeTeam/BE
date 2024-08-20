4eecf9b65df1ab57daff385c48bef4d9c1c6cfbf
fnzksxl <fnzksxl@gmail.com>
2024-08-21 02:13:56 +0900

    #80 Feat: Pagination Total Count -> Redis Cache 적용
    검색 기능을 활용할 때에는 캐싱 X

--- wtnt/core/pagenations.py
+++ wtnt/core/pagenations.py
@@ -1,5 +1,8 @@
 from rest_framework.pagination import PageNumberPagination, CursorPagination
+from django.core.paginator import Paginator
 import wtnt.settings as api_settings
+
+from django.core.cache import cache
 
 
 class ListPagenationSize10(PageNumberPagination):
@@ -22,8 +25,49 @@ class ListPagenationSize10(PageNumberPagination):
         return url.replace("http://localhost:8000", api_settings.MY_DOMAIN)
 
 
+class CachedPaginator(Paginator):
+    def __init__(self, *args, cache_key=None, cache_timeout=600, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cache_key = cache_key
+        self.cache_timeout = cache_timeout
+
+    @property
+    def count(self):
+        if self.cache_key:
+            cached_count = cache.get(self.cache_key)
+            if cached_count is not None:
+                return cached_count
+
+        count = super().count
+
+        if self.cache_key:
+            cache.set(self.cache_key, count, timeout=self.cache_timeout)
+
+        return count
+
+
+class UserListPagenationSize10(ListPagenationSize10):
+    def paginate_queryset(self, queryset, request, view=None, is_search=None):
+        self.request = request
+        cache_key = "" if is_search else "cached_count_user"
+        paginator = CachedPaginator(queryset, self.page_size, cache_key=cache_key, cache_timeout=60 * 10)
+        self.page = paginator.page(self.request.query_params.get("page", 1))
+
+        return list(self.page)
+
+
+class TeamListPagenationSize10(ListPagenationSize10):
+    def paginate_queryset(self, queryset, request, view=None, is_search=None):
+        self.request = request
+        cache_key = "" if is_search else "cached_count_team"
+        paginator = CachedPaginator(queryset, self.page_size, cache_key=cache_key, cache_timeout=60 * 10)
+        self.page = paginator.page(self.request.query_params.get("page", 1))
+
+        return list(self.page)
+
+
 class TeamPagination(CursorPagination):
-    page_size = 2
+    page_size = 4
     ordering = "-created_at"
 
     def get_next_link(self):
