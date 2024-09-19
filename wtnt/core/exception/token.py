7326bb64c1b816a284558473f77eaebdd32d7c4a
fnzksxl <fnzksxl@gmail.com>
2024-09-19 21:35:40 +0900

    Feat: Token Exception 상태코드 수정
    만료 -> 401, 변조 또는 형식 에러 -> 403

--- wtnt/core/exception/token.py
+++ wtnt/core/exception/token.py
@@ -1,31 +1,31 @@
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
 
 
 class AccessTokenExpiredError(AuthenticationFailed):
     default_detail = {"message": "만료된 액세스 토큰입니다.", "code": "0400"}
 
 
-class InvalidTokenError(AuthenticationFailed):
+class InvalidTokenError(PermissionDenied):
     default_detail = {"message": "유효한 토큰의 타입이 아닙니다.", "code": "0401"}
 
 
-class AuthenticationHeaderSpaceError(AuthenticationFailed):
+class AuthenticationHeaderSpaceError(PermissionDenied):
     default_detail = {"message": "인증 헤더는 반드시 두 덩이로 이루어져야합니다.", "code": "0402"}
 
 
-class TokenWithNoUserDataError(AuthenticationFailed):
+class TokenWithNoUserDataError(PermissionDenied):
     default_detail = {"message": "토큰에 유저의 식별정보가 들어있지 않습니다.", "code": "0403"}
 
 
-class UserNotFoundInTokenDataError(AuthenticationFailed):
+class UserNotFoundInTokenDataError(PermissionDenied):
     default_detail = {"message": "토큰의 유저 식별정보가 유효하지 않습니다.", "code": "0404"}
 
 
-class UserInactiveInTokenDataError(AuthenticationFailed):
+class UserInactiveInTokenDataError(PermissionDenied):
     default_detail = {"message": "토큰 속 유저가 Inactive 상태입니다.", "code": "0405"}
 
 
-class NoTokenInAuthorizationHeaderError(AuthenticationFailed):
+class NoTokenInAuthorizationHeaderError(PermissionDenied):
     default_detail = {"message": "자격 증명이 주어지지 않았습니다.", "code": "0406"}
 
 
